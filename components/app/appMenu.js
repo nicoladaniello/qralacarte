de46bf6281b12056d8142a31fee40055e17823a1
Nicola D'Aniello <nicola.danie@gmail.com>
2020-08-04 12:00:42 +0200

    bugfix - firebase connection

--- components/app/appMenu.js
+++ components/app/appMenu.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import MenuSection from "./MenuSection";
 import map from "lodash/map";
 
--- firebase/adminApp.js
+++ firebase/adminApp.js
@@ -1,12 +1,18 @@
 import * as admin from "firebase-admin";
 import "firebase/database";
 
-var serviceAccount = require("/Users/Nico/qralacarte/qralacarte-firebase-adminsdk-o1710-c6a967ee8b.json");
+// var serviceAccount = require("/Users/Nico/qralacarte/qralacarte-firebase-adminsdk-o1710-c6a967ee8b.json");
+
+const serviceAccount = {
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://qralacarte.firebaseio.com",
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   });
 }
