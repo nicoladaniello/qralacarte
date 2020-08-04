d78b8bef9193a83e05fb8c2f8eb5d0b251dccc66
Nicola D'Aniello <nicola.danie@gmail.com>
2020-08-04 12:12:25 +0200

    bugfix - firebase private key escape string

--- firebase/adminApp.js
+++ firebase/adminApp.js
@@ -5,7 +5,7 @@ import "firebase/database";
 
 const serviceAccount = {
   project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
 };
 
