e1fe65281a64eaadcd18161d0caa4cca7e29541d
nicoladaniello <nicola.danie@gmail.com>
2021-06-22 12:22:10 +0100

    bugfix: remove testing import

--- firebase/db/importer.js
+++ firebase/db/importer.js
@@ -1,28 +1,28 @@
-import firebase from "../firebaseAdmin";
-import aragosta from "./db-aragosta-22-07-22.json";
+// import firebase from "../firebaseAdmin";
+// import aragosta from "./db-aragosta-22-07-22.json";
 
-var db = firebase.firestore();
+// var db = firebase.firestore();
 
-async function importer() {
-  const menuRef = db.collection("menus").doc("ristorante-laragosta");
-  const infoRef = menuRef.collection("public").doc("info");
-  const sectionsRef = menuRef.collection("sections");
-  const productsRef = menuRef.collection("products");
+// async function importer() {
+//   const menuRef = db.collection("menus").doc("ristorante-laragosta");
+//   const infoRef = menuRef.collection("public").doc("info");
+//   const sectionsRef = menuRef.collection("sections");
+//   const productsRef = menuRef.collection("products");
 
-  const infoPromise = infoRef.set(
-    { sectionIds: aragosta.sectionIds },
-    { merge: true }
-  );
+//   const infoPromise = infoRef.set(
+//     { sectionIds: aragosta.sectionIds },
+//     { merge: true }
+//   );
 
-  const sectionPromises = Object.keys(aragosta.sections).map((sid) =>
-    sectionsRef.doc(sid).set(aragosta.sections[sid])
-  );
+//   const sectionPromises = Object.keys(aragosta.sections).map((sid) =>
+//     sectionsRef.doc(sid).set(aragosta.sections[sid])
+//   );
 
-  const productsPromises = Object.keys(aragosta.products).map((pid) =>
-    productsRef.doc(pid).set(aragosta.products[pid])
-  );
+//   const productsPromises = Object.keys(aragosta.products).map((pid) =>
+//     productsRef.doc(pid).set(aragosta.products[pid])
+//   );
 
-  await Promise.all([infoPromise, ...sectionPromises, ...productsPromises]);
-}
+//   await Promise.all([infoPromise, ...sectionPromises, ...productsPromises]);
+// }
 
-export default importer;
+// export default importer;
